9c45115ba91f4a0fecb23e54214db82de68a915d
MoeBrowne <oliver.browne@uplyme.com>
2016-10-28 21:32:08 +0100

    Added a check to see if the auth JSON could be parsed correctly

--- auth.php
+++ auth.php
@@ -5,6 +5,10 @@ use Bitbucket\API\Http\Listener\OAuthListener;
 
 $credentials = json_decode(file_get_contents('auth.json'), true);
 
+if (json_last_error() !== JSON_ERROR_NONE) {
+    throw new Exception("ERROR: ".json_last_error_msg());
+}
+
 $bitbucket = new Api();
 $bitbucket->getClient()->setApiVersion('2.0')->addListener(
     new OAuthListener($credentials)
