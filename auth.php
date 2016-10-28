302ba22aa6f2725220de13686b817d5aacee8a38
MoeBrowne <oliver.browne@uplyme.com>
2016-10-28 21:33:17 +0100

    Threw an exception when the auth.json could not be found, fixing #1

--- auth.php
+++ auth.php
@@ -3,7 +3,13 @@
 use Bitbucket\API\Api;
 use Bitbucket\API\Http\Listener\OAuthListener;
 
-$credentials = json_decode(file_get_contents('auth.json'), true);
+$authFileName = 'auth.json';
+
+if (!file_exists($authFileName)) {
+    throw new Exception("ERROR: auth.json is missing.");
+}
+
+$credentials = json_decode(file_get_contents($authFileName), true);
 
 if (json_last_error() !== JSON_ERROR_NONE) {
     throw new Exception("ERROR: ".json_last_error_msg());
